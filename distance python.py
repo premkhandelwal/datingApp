5c53bb29d92ee9dae6a0a386553ff7664fa16309
mayuresh-surve <me.mayuresh.99@gmail.com>
2021-10-01 17:38:52 +0530

    ✨ added python file to calculate distance.

---
+++ distance python.py
@@ -0,0 +1,30 @@
+from math import radians, cos, sin, asin, sqrt
+def distance(lat1, lat2, lon1, lon2):
+     
+    # The math module contains a function named
+    # radians which converts from degrees to radians.
+    lon1 = radians(lon1)
+    lon2 = radians(lon2)
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
+      
+    # Haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+ 
+    c = 2 * asin(sqrt(a))
+    
+    # Radius of earth in kilometers. Use 3956 for miles
+    r = 6371
+      
+    # calculate the result
+    return(c * r)
+     
+     
+# driver code
+lat1 = 37.4219983
+lat2 = 18.6193948
+lon1 = -122.084
+lon2 =  73.8575275
+print(distance(lat1, lat2, lon1, lon2), "K.M")
